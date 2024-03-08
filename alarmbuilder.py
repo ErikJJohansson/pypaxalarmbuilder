023d96b7c0a75fd2d38d4f580f832fec24653b79
Erik Johansson <ejohansson@eh2.com>
2024-03-08 14:47:17 -0800

    Rename extension to .xlsx

--- alarmbuilder.py
+++ alarmbuilder.py
@@ -1,7 +1,18 @@
-import pycomm3
+from pycomm3 import LogixDriver
+from sys import argv
+import openpyxl
+from tqdm import trange, tqdm
+from itertools import product
+import argparse
+
 
 # Input parameters, PLC IP address, Device Shortcut (/::[Pilot]) etc.
 
+# static list of alarm messages, this gets written into messages sheet
+msg_list = ['',
+            'TEST ALARM NEEDS TAG;  Val=/*N:5 %Tag1 NOFILL DP:1*/;'
+
+]
 
 
 # open up template csv
@@ -18,3 +29,59 @@ import pycomm3
 
 
 # Poke PLC for L_ModuleSts and make alarms for those modules
+
+def main():
+
+    # default filename of template file included in the repo
+    template_excelfile = 'FTAE_AlarmExport.xlsx'
+    
+    # will be replaced with PLC name
+    default_deviceshortcut = ''
+
+    # Parse arguments
+   
+    parser = argparse.ArgumentParser(
+        description='Python-based PlantPAX alarm builder tool',
+        epilog='This tool works on both Windows and Mac.')
+    
+    # Add command-line arguments
+    parser.add_argument('commpath', help='Path to PLC')
+    parser.add_argument('deviceshortcut', nargs='?', default=default_deviceshortcut,help='Shortcut in FTView')
+                                       
+    args = parser.parse_args()
+
+    # Access the parsed arguments
+    commpath = args.commpath
+    device_shortcut = args.deviceshortcut
+
+    # open connection to PLC
+
+    plc = LogixDriver(commpath, init_tags=True,init_program_tags=True)
+
+    print('Connecting to PLC.')
+    try:
+        plc.open()
+        plc_name = plc.get_plc_name()
+
+        print('Connected to ' + plc_name + ' PLC at ' + commpath)
+    except:
+        print('Unable to connect to PLC at ' + commpath)
+        exit()
+
+    # if the shortcut was left blank, create it
+    if device_shortcut == '':
+        device_shortcut = '/::[' + plc_name + ']'
+
+    # open excel file
+
+    # filename check
+    print('Opening ' + template_excelfile)
+    try:
+        book = openpyxl.load_workbook(template_excelfile,keep_vba=False,keep_links=True)
+
+    except:
+        print('Unable to open excel file ' + template_excelfile)
+        plc.close()
+        exit()
+    
+    print('Opened file named ' + template_excelfile)
