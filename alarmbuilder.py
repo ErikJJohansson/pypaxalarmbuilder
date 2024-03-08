5e2b65df3ee506bb9324e6ccdd0c8214d1cda1aa
Erik Johansson <ejohansson@eh2.com>
2024-03-08 11:04:58 -0800

    Add comments to file

---
+++ alarmbuilder.py
@@ -0,0 +1,20 @@
+import pycomm3
+
+# Input parameters, PLC IP address, Device Shortcut (/::[Pilot]) etc.
+
+
+
+# open up template csv
+# maybe use template xml for each alarm type?
+
+# write array of alarm messages to template csv file
+
+# Poke PLC for P_AIn, P_AIChan, P_AInDual, P_AInMulti
+# make sure it works for program scope tags
+
+# Each datatype has alarms, write those alarms to spreadsheet based on tag name
+
+# alarm group is name of PLC?
+
+
+# Poke PLC for L_ModuleSts and make alarms for those modules
